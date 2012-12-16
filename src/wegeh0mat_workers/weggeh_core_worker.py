51091f4df0809a59c708dd43eada776cbd042153
Peter Dahlberg <catdog2@tuxzone.org>
2012-12-16 07:53:09 +0100

    zwischenstand

--- src/wegeh0mat_workers/weggeh_core_worker.py
+++ src/wegeh0mat_workers/weggeh_core_worker.py
@@ -6,7 +6,7 @@ import datetime
 import re
 from datetime import date
 
-participant_status_to_text = {"yes" : "Ja", "maybe" : "Vieleicht", "no" : "Nein", "invited" : "Eingeladen"}
+participant_status_to_text = {"yes" : "Ja", "maybe" : "Vielleicht", "no" : "Nein", "invited" : "Eingeladen"}
 
 class WeggehWorker(worker.Worker):
     def __init__(self, config):
@@ -21,9 +21,14 @@ class WeggehWorker(worker.Worker):
         # self._db_connection.commit()
         # c.close()
         
+
+    
+    
+    
+    
     def handle_command(self, origin, msg : dict, command : str, sendername : str, senderjid: str):
-        if util.starts_with_one_of(command.lower(), 'hilfe'):
-            origin.handle_reply(msg, "%s: Dir ist nicht zu helfen!" % sendername)
+        if util.starts_with_one_of(command.lower(), 'hilfe', '?'):
+            origin.handle_reply(msg, self._get_help_message())
         elif util.starts_with_one_of(command.lower(), 'zeige alle vorschläge im detail'):
             origin.handle_reply(msg, self._get_event_list_str(all=True, details=True))
         elif util.starts_with_one_of(command.lower(), 'zeige alle vorschläge'):
@@ -32,6 +37,55 @@ class WeggehWorker(worker.Worker):
             origin.handle_reply(msg, self._get_event_list_str(all=False, details=True))
         elif util.starts_with_one_of(command.lower(), 'zeige vorschläge'):
             origin.handle_reply(msg, self._get_event_list_str(all=False, details=False))
+        elif util.starts_with_one_of(command.lower(), 'einladung an'):
+            username = None
+            id = None
+            try:
+                p = re.compile('.*einladung\s+an\s+(.+)\s+zu\s+vorschlag\s+([0-9]+)')
+                match = p.match(command.lower())
+                username = match.groups()[0]
+                id = int(match.groups()[1])
+            except Exception as e:
+                logging.getLogger().debug("unable to parse setze name: %s" % str(e))
+                return            
+            origin.handle_reply(msg, self._handle_invite(id, username))
+        elif util.starts_with_one_of(command.lower(), 'setze name'):
+            id = None
+            name = None
+            try:
+                p = re.compile('.*setze\s+name\s+von\s+vorschlag\s+([0-9]+)\s+auf\s+(.+)')
+                match = p.match(command.lower())
+                id = int(match.groups()[0])
+                name = match.groups()[1]
+            except Exception as e:
+                logging.getLogger().debug("unable to parse setze name: %s" % str(e))
+                return            
+            origin.handle_reply(msg, self._handle_name_change(id, name))
+        elif util.starts_with_one_of(command.lower(), 'setze treffpunkt'):
+            id = None
+            meeting_point = None
+            try:
+                p = re.compile('.*setze\s+treffpunkt\s+in\s+vorschlag\s+([0-9]+)\s+auf\s+(.+)')
+                match = p.match(command.lower())
+                id = int(match.groups()[0])
+                meeting_point = match.groups()[1]
+            except Exception as e:
+                logging.getLogger().debug("unable to parse setze treffpunkt: %s" % str(e))
+                return            
+            origin.handle_reply(msg, self._handle_meeting_point_change(id, meeting_point))
+        elif util.starts_with_one_of(command.lower(), 'setze kommentar'):
+            id = None
+            comment = None
+            try:
+                p = re.compile('.*setze\s+kommentar\s+in\s+vorschlag\s+([0-9]+)\s+auf\s+(.+)')
+                match = p.match(command.lower())
+                id = int(match.groups()[0])
+                comment = match.groups()[1]
+            except Exception as e:
+                logging.getLogger().debug("unable to parse setze kommentar: %s" % str(e))
+                return            
+            origin.handle_reply(msg, self._handle_comment_change(id, comment))
+            
         elif util.starts_with_one_of(command.lower(), 'setze status in vorschlag'):
             id = None
             status = None
@@ -40,14 +94,14 @@ class WeggehWorker(worker.Worker):
                 match = p.match(command.lower())
                 id = int(match.groups()[0])
                 psttreversed = dict([(participant_status_to_text[k].lower(), k) for k in participant_status_to_text])
-                if status not in psttreversed:
+                status = psttreversed[match.groups()[1]]
+                if status not in ["yes", "no", "maybe"]:
                     origin.handle_reply(msg, "Ungültiger status!")
                     return
-                status = psttreversed[match.groups()[1]]
             except Exception as e:
                 logging.getLogger().debug("unable to parse setze status: %s" % str(e))
                 return            
-            origin.handle_reply(msg, self.__handle_status_change(self, id, status, sendername, senderjid))
+            origin.handle_reply(msg, self._handle_status_change(id, status, sendername, senderjid))
         elif util.starts_with_one_of(command.lower(), 'neuer vorschlag'):
             
             date = None
@@ -64,30 +118,111 @@ class WeggehWorker(worker.Worker):
                 return    
             
             origin.handle_reply(msg, self._handle_vorschlag(name, date, sendername, senderjid))
-
-
-    def _handle_status_change(self, eventid, status, sendername, senderjid):
-        c = self._db_connection.cursor()
-        c.execute("select id from people where nickname = ? or jid = ?", (sendername, senderjid))
-        nameresult = c.fetchone()
-        userid = -1
-        
-        if nameresult == None:
             
+    def _handle_invite(self, eventid, username):
+        c = self._db_connection.cursor()
+        c.execute("select id from event where id = ?", (eventid,))
+        if(c.fetchone() == None):
+            return "Vorschlag exitiert nicht!"
+        
+        c.execute("select id from people where nickname = ?", (username,))
+        nameresult = c.fetchone()
+        userid = -1        
+        
+        if nameresult == None:            
             c = self._db_connection.cursor()
             c.execute("select max(id) from people")
-            userid = c.fetchone()
-            creatorid = maxresult[0] + 1
-            c.execute("insert  into people (id,nickname,jid) values(?,?,?)", (creatorid, sendername, senderjid))
+            maxresult = c.fetchone()
+            userid = maxresult[0] + 1
+            c.execute("insert  into people (id,nickname,jid) values(?,?,null)", (userid, username))
+            c.execute("insert into participants (event_id, people_id, status)" + 
+                      "values(?,?,'invited')", (eventid , userid)) 
             
         else:
             userid = nameresult[0]
+            c.execute("update participants set status='invited' " + 
+                      " where event_id=? and people_id=?", (eventid , userid))
+        
+        self._db_connection.commit()
+        c.close()
+        
+        return "%s zu Vorschlag #%d eingeladen!" % (username, eventid)
+        
+    def _get_help_message(self):
+        m = "Folgende Kommandos sind verfügbar:\n"
+        m += "zeige alle vorschläge im detail\n"
+        m += "zeige alle vorschläge\n"
+        m += "zeige vorschläge im detail\n"
+        m += "zeige vorschläge\n"
+        m += "neuer vorschlag <name> am <monat.jahr> um <stunde:minute>\n"
+        m += "setze status in vorschlag <#vorschlag> auf <ja|nein|vielleicht>\n"
+        m += "setze kommentar in vorschlag <#vorschlag> auf <kommentar>\n"
+        m += "setze name von vorschlag <#vorschlag> auf <neuer name>\n"
+        m += "einladung an <nickname> zu vorschlag <#vorschlag>\n"
+        return m.rstrip("\n")
+        
+    def _handle_name_change(self, id, name):
+        c = self._db_connection.cursor()
+        c.execute("select id from event where id = ?", (id,))
+        if(c.fetchone() == None):
+            return "Vorschlag exitiert nicht!"
+        
+        c.execute("update event set name=? where id = ?", (name, id))
+        self._db_connection.commit()
+        c.close()
+        return "Name von Vorschlag #%d auf %s gesetzt!" % (id, name)
+    
+    def _handle_meeting_point_change(self, id, meeting_point):
+        c = self._db_connection.cursor()
+        c.execute("select id from event where id = ?", (id,))
+        if(c.fetchone() == None):
+            return "Vorschlag exitiert nicht!"
+        
+        c.execute("update event set meeting_point=? where id = ?", (meeting_point, id))
+        self._db_connection.commit()
+        c.close()
+        return "Treffpunkt für Vorschlag #%d auf %s gesetzt!" % (id, meeting_point)
+
+    def _handle_comment_change(self, id, comment):
+        c = self._db_connection.cursor()
+        c.execute("select id from event where id = ?", (id,))
+        if(c.fetchone() == None):
+            return "Vorschlag exitiert nicht!"
+        
+        c.execute("update event set comment=? where id = ?", (comment, id))
+        self._db_connection.commit()
+        c.close()
+        return "Kommentar für Vorschlag #%d auf %s gesetzt!" % (id, comment)
+
+    def _handle_status_change(self, eventid, status, sendername, senderjid):
+        c = self._db_connection.cursor()
+        c.execute("select id from event where id = ?", (eventid,))
+        if(c.fetchone() == None):
+            return "Vorschlag exitiert nicht!"
+        
+        c.execute("select id from people where nickname = ? or jid = ?", (sendername, senderjid))
+        nameresult = c.fetchone()
+        userid = -1        
+        
+        if nameresult == None:            
+            c = self._db_connection.cursor()
+            c.execute("select max(id) from people")
+            maxresult = c.fetchone()
+            userid = maxresult[0] + 1
+            c.execute("insert  into people (id,nickname,jid) values(?,?,?)", (userid, sendername, senderjid))
+            c.execute("insert into participants (event_id, people_id, status)" + 
+                      "values(?,?,?)", (eventid , userid, status)) 
             
-        c.execute("insert into participants (event_id, people_id, status)" + 
-                  "values(?,?,?)", (eventid , userid, status))        
+        else:
+            userid = nameresult[0]
+            c.execute("update participants set status=? " + 
+                      " where event_id=? and people_id=?", (status, eventid , userid))
+            
 
         self._db_connection.commit()
-        c.close() 
+        c.close()
+        
+        return "Status für Vorschlag #%d auf %s gesetzt!" % (eventid, participant_status_to_text[status])  
 
     def _handle_vorschlag(self, name: str, date: datetime.datetime, sendername, senderjid) -> str:
         c = self._db_connection.cursor()
